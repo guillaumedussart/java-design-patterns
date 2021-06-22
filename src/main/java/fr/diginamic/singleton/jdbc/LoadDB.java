32e49ec7bab1b250fd38ec1eb1174d91856f9a49
guillaume dussart <guillaume@localdev.local>
2021-06-22 10:36:07 +0200

    refact singleton

--- src/main/java/fr/diginamic/singleton/jdbc/LoadDB.java
+++ src/main/java/fr/diginamic/singleton/jdbc/LoadDB.java
@@ -13,22 +13,22 @@ public class LoadDB {
     private static final String DB_PWD;
 
     private Connection connection;
+
     static {
-        ResourceBundle bundle = ResourceBundle.getBundle( "configuration" );
-        DB_URL = bundle.getString( "db.url" );
-        DB_USER = bundle.getString( "db.user" );
-        DB_PWD = bundle.getString( "db.password" );
+        ResourceBundle bundle = ResourceBundle.getBundle("configuration");
+        DB_URL = bundle.getString("db.url");
+        DB_USER = bundle.getString("db.user");
+        DB_PWD = bundle.getString("db.password");
     }
 
     private LoadDB() throws SQLException {
-        connection = DriverManager.getConnection( DB_URL, DB_USER, DB_PWD );
+        connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PWD);
     }
 
     public static LoadDB getSingle() throws SQLException {
-        if (null == single) {
-            single = new LoadDB();
-        }
-        return single;
+
+        return single = new LoadDB();
+
     }
 
     public Connection getConnection() {
